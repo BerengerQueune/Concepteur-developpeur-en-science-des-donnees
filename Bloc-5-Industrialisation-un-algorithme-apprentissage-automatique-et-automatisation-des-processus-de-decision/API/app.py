8b3fe6975cdeeeb98305bc1d154b0a6f91d95da2
Berenger <bqueune@hotmail.com>
2022-05-27 17:15:09 +0200

    update

--- Bloc-5-Industrialisation-un-algorithme-apprentissage-automatique-et-automatisation-des-processus-de-decision/API/app.py
+++ Bloc-5-Industrialisation-un-algorithme-apprentissage-automatique-et-automatisation-des-processus-de-decision/API/app.py
@@ -49,7 +49,7 @@ class PredictionFeatures(BaseModel):
 @app.get("/")
 async def index():
 
-    message = "Hello this is my second API. If you want to learn more, check out documentation of the api at `/docs`"
+    message = "Hello this is my first API. If you want to learn more, check out documentation of the api at `/docs`"
 
     return message
 
@@ -74,12 +74,6 @@ async def predict(predictionFeatures: PredictionFeatures):
     return response
 
 
-# @app.get("/custom-greetings")
-# async def custom_greetings(name: str = "Mr (or Miss) Nobody"):
-#     greetings = {
-#         "Message": f"Hello {name} How are you today?"
-#     }
-#     return greetings
 
 if __name__=="__main__":
     uvicorn.run(app, host="0.0.0.0", port=4000) # Here you define your web server to run the `app` variable (which contains FastAPI instance), with a specific host IP (0.0.0.0) and port (4000)
